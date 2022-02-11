0b04b18bdee39cb4442cecc7f5dc4f48e0b04c6d
Dmitrii Kilishek <mentalclear@gmail.com>
2022-02-11 14:34:45 -0500

    Continue with Chapter 9

---
+++ chapt08/getstack/getstack.go
@@ -0,0 +1,3 @@
+package main
+
+//By default, the stack trace is not printed out. If you want to see the stack trace, use fmt.Printf and the verbose output verb (%+v). Check the documentation to learn more.
